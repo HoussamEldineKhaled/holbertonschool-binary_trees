d3f915b136c888aa68f4f4becfc7f73a23fefd57
root <houssam.khaled@lau.edu>
2025-02-06 17:27:35 +0000

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,17 +4,15 @@
 
 /**
  * binary_tree_size - find size of tree
- * @tree: tree 
+ * @tree: tree
  * Return: 0 or size
 */
-
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 size_t Lsize = 1;
 size_t Rsize = 1;
 if (tree == NULL)
- {
+{
 return (0);
 }
 Lsize = binary_tree_size(tree->left);

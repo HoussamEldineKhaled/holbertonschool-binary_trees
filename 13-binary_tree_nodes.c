af6d1bbd96990c1892095bf701b7542c9d8296a4
root <houssam.khaled@lau.edu>
2025-02-06 18:13:25 +0000

    to be continued

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+
+/**
+ * binary_tree_nodes - count regular connected nodes
+ * @tree: root of tree
+ * Return: 0, 1, or total
+*/
+
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+size_t count = 0;
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+{
+return (0);
+}
+if (tree->left == NULL && tree->right != NULL)
+{
+
+}
+return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}

920631d726fc38fda8d4b832f4b5da124114392f
root <houssam.khaled@lau.edu>
2025-02-06 17:00:01 +0000

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,15 +1,22 @@
 #include "binary_trees.h"
+#include <stddef.h>
 
 
-
+/**
+ * binary_tree_depth - find depth of tree
+ * @tree: given tree position
+ * Return: depth or 0
+*/
 
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  size_t depth = -1;
-  if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-    {
-      return (0);
-    }
-  
+size_t depth = 0;
+if (tree == NULL || tree->parent == NULL)
+{
+return (depth);
+}
+depth++;
+depth = binary_tree_depth(tree->parent);
+return (depth + 1);
 }

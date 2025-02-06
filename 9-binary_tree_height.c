3343e730eb0b3488119b652f755bf895c4bd4722
root <houssam.khaled@lau.edu>
2025-02-06 14:55:28 +0000

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t Lsize = 0;
 size_t Rsize = 0;
-if (tree == NULL)
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 {
-return (-1);
-}  
-Lsize = binary_tree_height(tree->left) + 1;
-Rsize = binary_tree_height(tree->right) + 1;
-return ((Lsize > Rsize ? Lsize : Rsize));
+return (0);
+}
+Lsize = binary_tree_height(tree->left);
+Rsize = binary_tree_height(tree->right);
+return ((Lsize > Rsize ? Lsize : Rsize) + 1);
 }
